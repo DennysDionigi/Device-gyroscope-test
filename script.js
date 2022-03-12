4231bd6f69d5700ac138be5d9e4d89de630b38f0
DennysDionigi <dennys.palumbo@gmail.com>
2022-03-12 02:46:03 +0100

    Update script.js
    Added mq: coarse only devices

--- script.js
+++ script.js
@@ -4,43 +4,47 @@ let vx = 0.0; // Velocità x & y
 let vy = 0.0;
 let updateRate = 1 / 60; // Frequenza di aggiornamento del sensore
 
-function getAccel() {
-  DeviceMotionEvent.requestPermission().then((response) => {
-    if (response == "granted") {
-      // Aggiungi un listener per ottenere l'orientamento dello smartphone
-      // negli assi alfa-beta-gamma (in gradi)
-      window.addEventListener("deviceorientation", (event) => {
-// Espone ogni angolo di orientamento in modo più leggibile
-        rotation_degrees = event.alpha;
-        frontToBack_degrees = event.beta;
-        leftToRight_degrees = event.gamma;
+/*coarse only = mobile */
+if (window.matchMedia("(pointer: coarse)").matches) {
 
-        // Aggiorna la velocità in base all'inclinazione del telefono
-        // Poiché i telefoni sono più stretti che lunghi, raddoppia l'aumento della velocità x - per un tablet occorre uno switch di casi
-        vx = vx + leftToRight_degrees * updateRate * 2;
-        vy = vy + frontToBack_degrees * updateRate;
+  function getAccel() {
+    DeviceMotionEvent.requestPermission().then(response => {
+      if (response == "granted") {
+        // Aggiungi un listener per ottenere l'orientamento dello smartphone
+        // negli assi alfa-beta-gamma (in gradi)
+        window.addEventListener("deviceorientation", event => {
+          // Espone ogni angolo di orientamento in modo più leggibile
+          rotation_degrees = event.alpha;
+          frontToBack_degrees = event.beta;
+          leftToRight_degrees = event.gamma;
 
-        // Aggiorna la posizione e agganciala ai limiti
-        px = px + vx * 0.5;
-        if (px > 98 || px < 0) {
-          px = Math.max(0, Math.min(98, px)); // Clip posiz x entro 0-98
-          vx = 0;
-        }
+          // Aggiorna la velocità in base all'inclinazione del telefono
+          // Poiché i telefoni sono più stretti che lunghi, raddoppia l'aumento della velocità x - per un tablet occorre uno switch di casi
+          vx = vx + leftToRight_degrees * updateRate * 2;
+          vy = vy + frontToBack_degrees * updateRate;
 
-        py = py + vy * 0.5;
-        if (py > 98 || py < 0) {
-          py = Math.max(0, Math.min(98, py)); // Clip posiz y entro 0-98
-          vy = 0;
-        }
+          // Aggiorna la posizione e agganciala ai limiti
+          px = px + vx * 0.5;
+          if (px > 98 || px < 0) {
+            px = Math.max(0, Math.min(98, px)); // Clip posiz x entro 0-98
+            vx = 0;
+          }
 
-        dot = document.getElementsByClassName("indicatorDot")[0];
-        dot.setAttribute("style", "left:" + px + "%;" + "top:" + py + "%;");
-      });
-    }
-  });
+          py = py + vy * 0.5;
+          if (py > 98 || py < 0) {
+            py = Math.max(0, Math.min(98, py)); // Clip posiz y entro 0-98
+            vy = 0;
+          }
+
+          dot = document.getElementsByClassName("indicatorDot")[0];
+          dot.setAttribute("style", "left:" + px + "%;" + "top:" + py + "%;");
+        });
+      }
+    });
+  }
+
+  /*evento al click*/
+  let btn = document.querySelector("#accelPermsButton");
+
+  btn.addEventListener("click", getAccel);
 }
-
-/*evento al click*/
-let btn = document.querySelector("#accelPermsButton");
-
-btn.addEventListener("click", getAccel );
